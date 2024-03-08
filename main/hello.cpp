adae50ba86e91f1540cc757d70637a1b165c1aa7
Sinchana Hebbar <sinchanahebbar15@gmail.com>
2024-03-08 21:37:45 +0530

    changes made

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, World!\n";
+  cout << "Hello, Jenkins! Welcome!\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
